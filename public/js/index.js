a9d353c897b7836ca9736f74a1f0a61216e0c96d
Shumpei Shiraishi <shumpei.shiraihsi@gmail.com>
2013-04-09 13:31:54 +0900

    とりあえずzapボタン機能するように

--- public/js/index.js
+++ public/js/index.js
@@ -5,10 +5,11 @@ $(function() {
             setTimeout(callback, 0);
         };
     }
-    var currentEvent;
-    var zapper = new Zapper();
-    var user;
-    var messages;
+    var currentEvent
+    , zapper = new Zapper()
+    , user
+    , messages
+    , $buttonsContainer = $('#buttons-container');
 
     function switchScreen(next, callback) {
         setTimeout(function() {
@@ -25,7 +26,7 @@ $(function() {
             init();
         }
     })();
-    $('#login-command').click(function(){
+    $('#login-command').fastClick(function(){
         zapper.login(null, function(err, user) {
             if (err) {
                 return;
@@ -44,11 +45,13 @@ $(function() {
             user = zapper.user;
             currentEvent = zapper.event(__lt_event__);
             $('#zapper-main h1:first').text(currentEvent.title);
-            /*
             currentEvent.onZapSent = function(zap) {
+                var backgroundUrl = 'url("/img/btnBg_00.png")';
+                $buttonsContainer.css('backgroundImage', backgroundUrl);
                 $('#zap-button').text('(いいね!)').removeAttr('disabled');
-                showMessageDialog('#zap-button', { relatedZapUUID: zap.uuid, autoCloseSeconds: 3 });
+//                showMessageDialog('#zap-button', { relatedZapUUID: zap.uuid, autoCloseSeconds: 3 });
             };
+            /*
             currentEvent.subscribe('zap', function(zap) {
                 zaps.push(zap);
             });
@@ -63,5 +66,41 @@ $(function() {
             */
         });
     }
-
+    (function preloadImages() {
+        var imgNames = [
+            'btnBg_00.png',
+            'btnBg_01.png',
+            'btnBg_02.png',
+            'btnBg_03.png',
+            'btnBg_04.png',
+            'btnBg_05.png',
+            'zapBtn_off.png',
+            'zapBtn_on.png',
+            'msg_on.png',
+            'msg_off.png',
+            'msgBtn_off.png',
+            'msgBtn_on.png'
+        ];
+        var pathPrefix = '/img/';
+        _.each(imgNames, function(imgName) {
+            var img = new Image();
+            img.src = pathPrefix + imgName;
+            img.onload = function() {
+                console.log('preload image:' + imgName);
+            };
+        });
+    })();
+    
+    $('#zap-button').fastClick(function() {
+        if (!zapper.loggedIn) {
+            return alert('ログインしていません');
+        }
+        var zap = currentEvent.zap();
+        var zapCount = zap.count;
+        if (zapCount === currentEvent.maxZapCount) {
+            $(this).attr('disabled', 'disabled');
+        }
+        var backgroundUrl = 'url("/img/btnBg_' + _.str.lpad(String(zapCount), 2, '0') + '.png")';
+        $buttonsContainer.css('backgroundImage', backgroundUrl);
+    });
 });
